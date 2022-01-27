c604b33561df529a84bee50ebc5449c023a388ab
Janne Välimaa <janne.valimaa@gmail.com>
2022-01-27 13:50:52 +0200

    Update Microsoft.PowerShell_profile.ps1
    Remove unused parts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,26 +1,13 @@
 #
-#   This profile is meant to run in wlsg Ubuntu.
-#
-
-#
 # Set prompt
 #
 function prompt {
-    $branch = git branch --show-current
-    if (-not [string]::IsNullOrEmpty($branch)) {
-        $origin = (git remote get-url origin).split('/', 3)[-1]
-        Write-Host "($origin) " -NoNewline -ForegroundColor Blue   
-        Write-Host "($branch) " -ForegroundColor Red
-    }
     Write-Host 'λ ' -NoNewline -ForegroundColor Green
     return ' '
 }
 #
-# Set "aliases". ls function requires: sudo cp /usr/bin/ls /usr/bin/ll
+# set commands
 #
-function edge {
-    Invoke-Expression microsoft-edge-beta &> /dev/null &
-}
 function ls ($loc = '.') {
     if (-not (which lp)) {
         Write-Host 'This command is overriden. I need sudo permission to make this work (I copy ls to lp)'
@@ -37,25 +24,3 @@ function venv {
         Write-Host 'No virtual environment folder'
     }
 }
-#
-# Common work tools I need
-#
-function Get-Tools {
-    if (-not (which teams)) {
-        Write-Host 'No teams installed. Download .deb)' -ForegroundColor Green
-        curl https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.4.00.7556_amd64.deb -o ~/Downloads/teams.deb
-        Write-Host 'I need sudo priv to run apt install teams.deb' -ForegroundColor Green
-        sudo apt install $env:HOME/Downloads/teams.deb
-    }
-    if (-not (which az)) {
-        Write-Host 'No az-cli installed. Run install script)' -ForegroundColor Green
-        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-    }
-    if (-not (which dotnet)) {
-        Write-Host 'No dotnet installed. Install from apt (teams install should have added apt)' -ForegroundColor Green
-        sudo apt install -y dotnet-sdk-5.0
-    }
-}
-
-# Set starting point to home
-Set-Location
